1a075b4d2bf43b0bc1b035138b7e03f601a584c8
Coleman Lindquist <coleman.lindquist.32@gmail.com>
2020-09-11 08:54:35 -0600

    node-bcrypt-afternoon

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const session = require('express-session')
+const massive = require('massive')
+require('dotenv').config()
+
+const PORT = 4000
+const { SESSION_SECRET, CONNECTION_STRING} = process.env
+const app = express()
+const authCtrl = require('./controllers/authController')
+const treasureCtrl = require('./controllers/treasureController')
+const auth = require('./middleware/authMiddleware')
+
+app.use(express.json())
+
+app.post('/auth/register', authCtrl.register)
+app.post('/auth/login', authCtrl.login)
+app.get('/auth/logout', authCtrl.logout)
+app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure)
+app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure)
+app.post('/api/treasure/user', auth.usersOnly, treasureCtrl.addUserTreasure)
+app.get('/api/treasure/all', auth.usersOnly, auth.adminsOnly, treasureCtrl.getAllTreasure)
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: { rejectUnauthorized: false}
+}).then(
+    db => {
+        app.set('db', db)
+        console.log('DB is here')
+        
+        app.listen(PORT, () => 
+        console.log(`Listening on port ${PORT}`)
+)
+    }
+)
+
+app.use(
+    session({
+        resave: true,
+        saveUninitialized: false,
+        secret: SESSION_SECRET
+    })
+)
+
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import './Header.css';
 
 export default class Header extends Component {
@@ -32,11 +33,28 @@ export default class Header extends Component {
   }
 
   register() {
-    // axios POST to /auth/register here
+    const { username, password, isAdmin } = this.state
+
+    axios
+      .post('/auth/register', { username, password, isAdmin })
+      .then(user => {
+        this.setState({ username: '', password: '' })
+        this.props.updateUser(user.data)
+      })
+
+      .catch(err => {
+        this.setState({ username: '', password: '' })
+        alert(err.response.request.response)
+      })
   }
 
   logout() {
-    // axios GET to /auth/logout here
+    axios
+    .get('/auth/logout')
+    .then(() => {
+      this.props.updateUser({})
+    })
+    .catch(err => console.log(err))
   }
 
   render() {
